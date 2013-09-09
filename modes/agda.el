ffb421c3530f617a51497f671a6269dcc573ce84
Praveen Narayanan <revenap@gmail.com>
2013-09-09 15:31:59 -0400

    Agda mode OS fix

--- modes/agda.el
+++ modes/agda.el
@@ -3,7 +3,6 @@
 (when (and (eq system-type 'darwin)
 	   (eq (user-login-name) "praveennarayanan"))
   (customize-set-variable 
-   'agda2-include-dirs (quote ("/Users/praveennarayanan/lib-0.6/src" "."))))
-
-(load-file (let ((coding-system-for-read 'utf-8))
-	     (shell-command-to-string "agda-mode locate")))
+   'agda2-include-dirs (quote ("/Users/praveennarayanan/lib-0.6/src" ".")))
+  (load-file (let ((coding-system-for-read 'utf-8))
+	       (shell-command-to-string "agda-mode locate"))))
